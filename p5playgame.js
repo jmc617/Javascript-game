3f4124d7799dba625308bf5963096727f720b0e1
jmc617 <jmc617@gmail.com>
2018-05-16 17:39:01 -0400

    directions added

--- p5playgame.js
+++ p5playgame.js
@@ -1,10 +1,9 @@
-//scoreboard element
-var scoreBoard = document.getElementById('score')
+// starting score and time
 var score = 0
 var time = 30
 gameOver = false
 
-//timer function
+// timer function
 setInterval(function timer(){
   time --
   if (time <= 0) {
@@ -26,16 +25,35 @@ function setup() {
   var can = createCanvas(800, 550);
   //numbers in paratheses distance from (left,top)
   // and size of sprite(50x50)
+  //player sprite
   square = createSprite(500, 500, 50, 50);
+  //barrier sprites
+
   rectangle = createSprite(300,75,20,100);
-  rectangle2 = createSprite(400,200,100,20);
-  rectangle3 = createSprite(500,75,20,100);
+  rectangle2 = createSprite(500,75,20,100);
+  rectangle3 = createSprite(400,200,100,20);
+  rectangle4 = createSprite(700,75,20,100);
+  rectangle5 = createSprite(600,200,100,20);
+  rectangle6 = createSprite(100,75,20,100);
+  rectangle7 = createSprite(200,200,100,20);
+
+  rectangle8 = createSprite(300,350,20,100);
+  rectangle9 = createSprite(500,350,20,100);
+  rectangle10 = createSprite(400,475,100,20);
+  rectangle11 = createSprite(700,350,20,100);
+  rectangle12 = createSprite(600,475,100,20);
+  rectangle13 = createSprite(100,350,20,100);
+  rectangle14 = createSprite(200,475,100,20);
+
+  //point sprites
   littleSquare = createSprite(600,300,25,25);
   littleSquare2 = createSprite(200,300,25,25);
 }
 
 function draw() {
-  //.rectangle3.shapeColor = 'color'
+  square.shapeColor = 'teal'
+  littleSquare.shapeColor = 'aqua'
+  littleSquare2.shapeColor = 'aqua'
   background(0, 10, 0);
   //new game on any key pressed after game over
   if (mouseIsPressed && gameOver) {
@@ -55,6 +73,8 @@ function draw() {
     textAlign(CENTER)
     textSize(20)
     text('Current Score is: '+score+' Time Left: '+time, 400, 20);
+    textSize(18)
+    text('Move square using arrow keys. Hit the tiny squares as many times as possible before time runs out' , 400, 540);
 
     //keeps player velocity/speed from getting too high
     square.maxSpeed = 3
@@ -106,15 +126,29 @@ function draw() {
     square.collide(rectangle);
     square.collide(rectangle2);
     square.collide(rectangle3);
+    square.collide(rectangle4);
+    square.collide(rectangle5);
+    square.collide(rectangle6);
+    square.collide(rectangle7);
+    square.collide(rectangle8);
+    square.collide(rectangle9);
+    square.collide(rectangle10);
+    square.collide(rectangle11);
+    square.collide(rectangle12);
+    square.collide(rectangle13);
+    square.collide(rectangle14);
 
     //gets new coordinates for littleSquares after they are hit
     function updateCoordinates() {
-      littleSquare.position.y = floor(random(10, (width - 100) / 10)) * 10;
-      littleSquare.position.x = floor(random(10, (height - 100) / 10)) * 10;
+      littleSquare.position.y = floor(random(10, (width) / 10)) * 10;
+      console.log(width);
+      console.log(floor(random(10, (width - 100) / 10)) * 10);
+      littleSquare.position.x = floor(random(10, (height) / 10)) * 10;
     }
     function updateCoordinates2() {
-      littleSquare2.position.y = floor(random(10, (width - 100) / 10)) * 10;
-      littleSquare2.position.x = floor(random(10, (height - 100) / 10)) * 10;
+      littleSquare2.position.y = floor(random(10, (width) / 10)) * 10;
+      console.log(floor(random(10, (width - 100) / 10)) * 10);
+      littleSquare2.position.x = floor(random(10, (height) / 10)) * 10;
     }
 
     //if littleSquares random position places them outside of the canvas it will update again
@@ -139,5 +173,3 @@ function draw() {
     drawSprites();
   }
 }
-
-//example of adding image to sprite circle.addImage(loadImage("assets/plain_circle.png"));
